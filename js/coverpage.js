e0dd01898270253ce2befa7b4752fe55ece4e929
Amina Lazrak <amina.lazrak@gmail.com>
2018-04-18 22:55:30 -0400

    Edit notebook page

--- js/coverpage.js
+++ js/coverpage.js
@@ -1,13 +1,13 @@
 
-var sorts = document.getElementsByClassName("button");
-console.log(sorts);
- for (var i=0; i< sorts.length; i++) {
- 	sorts[i].addEventListener("click",function(){
- 		var current = document.getElementsByClassName("active");
- 		current[0].className=current[0].className.replace("active","");
- 		this.className += "active";
- 	});
- };
+// var sorts = document.getElementsByClassName("button");
+// console.log(sorts);
+//  for (var i=0; i< sorts.length; i++) {
+//  	sorts[i].addEventListener("click",function(){
+//  		var current = document.getElementsByClassName("active");
+//  		current[0].className=current[0].className.replace("active","");
+//  		this.className += "active";
+//  	});
+//  };
 
 
 $(document).ready(function(){
@@ -20,11 +20,7 @@ $(document).ready(function(){
     newCount=0;
     $(".new").on("click", function(){
     	$(".project-content-main").append(
-    		$("<div class='project-rows'>").append(
-    		$("<div class='project-outer'>").append(
-    			$("<div class='project-inner'>").append(
-    				$("<a class='notebook-name' href='notebook_page_2.html'><h3>Bmal1: Circadian Rhythm</h3></a>").append(
-    					$("<span class='update'>Last Updated: </span>"))))));
+    		$('<div class="project-rows"><div class="project-outer" style="background-color: #3498db;"> <div class="project-inner"> <a class="notebook-name" href="notebook_page_2.html"><h3>New Project</h3></a> <span class="last-updated">Updated: Todays Date</span> </div> </div>'));
     	newCount=newCount+1;
 	});
 
@@ -67,10 +63,11 @@ function addProjectsByTitle(){
 };
 function addProjectsByDate(){
 	for (var p=0; p<projectDate.length; p++){
-		projectDate[p]=new Date(projectDate[p]);
 		// console.log(projectDate[p]);
 		for (var i=0; i<projectInfo.length; i++){
-			if ((projectDate[p])=== new Date(projectInfo[i].date)){
+			formattedProjectInfoDate=new Date(projectInfo[i].date).getTime();
+			// console.log(projectDate[p],formattedProjectInfoDate);
+			if ((projectDate[p])=== formattedProjectInfoDate){
 				$(".add").append('<div class="project-outer" style="background-color: '+projectInfo[i].color+';"> <div class="project-inner"> <a class="notebook-name" href="notebook_page_2.html"><h3>'+projectInfo[i].title+'</h3></a> <span class="last-updated">Updated: '+projectInfo[i].date+'</span> </div> </div>');
 			};
 		};
@@ -84,6 +81,8 @@ $(".title-sort").on("click", function(){
 	projectTitle.sort();
 	$(".add").empty();
 	addProjectsByTitle();
+	$(".title-sort").addClass("active");
+	$(".date-sort").removeClass("active");
 
 });
 
@@ -91,6 +90,8 @@ $(".date-sort").on("click", function(){
 	projectDate.sort();
     $(".add").empty();
 	addProjectsByDate();
+	$(".date-sort").addClass("active");
+	$(".title-sort").removeClass("active");
 })
 ///////////	
 
